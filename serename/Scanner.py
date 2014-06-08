02a18235af6cb9f7bc0a9ffcd5b3259dd3dba5c7
Ruben Smets <smets.ruben@gmail.com>
2014-06-08 20:06:25 +0200

    Created function remove_filename_extension - Wrote function logic to remove the extension from common video files (remove_filename_extension) - Wrote tests for remove_filename_extension

--- serename/Scanner.py
+++ serename/Scanner.py
@@ -3,5 +3,17 @@ class Scanner:
     def __init__(self):
         pass
 
-    def remove_filename_extensions(self):
-        return True
+    def remove_filename_extension(self, f):
+        filename = ""
+        if f[-4:].lower() in [".avi", ".mp4", ".mkv"]:
+            filename = f[0:-4]
+        elif f[-4:].lower() in [".srt", ".sub"]:
+            if f[-7:-4].lower() in [".en", ".nl"]:
+                filename = f[0:-7]
+            else:
+                filename = f[0:-4]
+
+        return filename
+
+    def scan_directory(self, directory):
+        return False
--- serename/test/test_scanner.py
+++ serename/test/test_scanner.py
@@ -1,19 +1,73 @@
+import os
+import shutil
+
+from serename.Scanner import Scanner
 from unittest import TestCase
 from unittest import TestSuite
 from unittest import TestLoader
-from serename.Scanner import Scanner
 
 
 class TestScanner(TestCase):
 
+    TEST_DIR = "test_data"
+    EPISODES = [
+        "The.Walking.Dead.avi",
+        "The.Walking.Dead.mkv",
+        "The.Walking.Dead.mp4",
+        "The.Walking.Dead.en.srt",
+        "The.Walking.Dead.srt",
+        "The.Walking.Dead.en.sub",
+        "The.Walking.Dead.sub"
+    ]
+    EPISODES_EXPECTED = [
+        "The.Walking.Dead"
+    ]
+
     def setUp(self):
+        os.mkdir(TestScanner.TEST_DIR)
         self.scanner = Scanner()
         pass
 
-    def test_remove_filename_extensions(self):
-        self.assertTrue(self.scanner.remove_filename_extensions(), "Yay!")
-
     def tearDown(self):
+        shutil.rmtree(TestScanner.TEST_DIR)
+        pass
+
+    def test_remove_filename_extension_avi(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[0])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_mkv(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[1])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_mp4(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[2])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_langsrt(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[3])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_srt(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[4])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_langsub(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[5])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_remove_filename_extension_sub(self):
+        result = self.scanner.remove_filename_extension(TestScanner.EPISODES[6])
+        expected = TestScanner.EPISODES_EXPECTED[0]
+        self.assertEqual(result, expected, "Got: " + result + ", Expected: " + expected)
+
+    def test_scan_directory(self):
         pass
 
 
