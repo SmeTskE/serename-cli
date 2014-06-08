f5777f73c30926c25defb97d4be5f096a8bff784
Ruben Smets <smets.ruben@gmail.com>
2014-06-08 20:19:56 +0200

    Added __init__.py again for Testing

---
+++ serename/test/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Ruben'
+
+from unittest import TestSuite
+from unittest import TextTestRunner
+from . import test_scanner
+
+suite = TestSuite([test_scanner.suite()])
+
+if __name__ == '__main__':
+    TextTestRunner().run(suite)
