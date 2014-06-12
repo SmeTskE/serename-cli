ce0fabd08bcd9a49cadebf21f2e705bc288b9c7c
Ruben Smets <ruben@cleardigital.be>
2014-06-12 20:17:44 +0200

    Switched to py.test

---
+++ serename/test/__init__.py
@@ -0,0 +1,11 @@
+__author__ = 'Ruben'
+
+# from unittest import TestSuite
+# from unittest import TextTestRunner
+#
+# import test_scanner
+#
+# suite = TestSuite([test_scanner.suite()])
+#
+# if __name__ == '__main__':
+#     TextTestRunner().run(suite)
--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name="serename",
     version="0.0.1-dev",
-    description="",
+    description="Python command line tool to batch rename tv shows.",
     long_description="",
     author="Ruben Smets",
-    packages=find_packages(),
+    packages=find_packages(exclude=['test']),
     install_requires=open("requirements.txt").readlines(),
     test_suite="serename.test.suite"
 )
